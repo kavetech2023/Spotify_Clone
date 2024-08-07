c604533a937ee19b44406cc6f083b9ac67ab4b2b
kavetech2023 <kavetech@gmail.com>
2024-08-07 22:50:30 +0300

    chore: Add Tailwind CSS configuration and dependencies

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    <div className='h-10 bg-black w-20 '>
+          <div className='h-[90%] flex'>
+            <Sidebar />
+          </div>
+    </div>
+  )
+}
+
+export default App
