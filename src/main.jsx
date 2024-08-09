8a99ba1201515256c9b207e131907e099c78b0a8
kavetech2023 <kavetech@gmail.com>
2024-08-09 11:08:02 +0300

    chore: Add lazy loading for images and update PlayerContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import PlayerContextProvider from './context/PlayerContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <PlayerContextProvider>
       <App />
+    </PlayerContextProvider> 
     </BrowserRouter>
   </React.StrictMode>,
 )
