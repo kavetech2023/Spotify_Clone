e96a771e354600a10fe0f1c18fc086e149ade6e6
kavetech2023 <kavetech@gmail.com>
2024-08-09 11:09:03 +0300

    chore: Update Navbar component styling and add lazy loading for images

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,13 +12,17 @@ const Navbar = () => {
                </span>
                <span onClick={navigate(1)}><img src='https://via.placeholder.com/150' alt='logo' className='w-8 bg-black p-2 rounded-2xl cursor-pointer'/>
                </span>  
-            </div>
-            <div className='flex items-center gap-4 '>
+        </div>
+
+
+        <div className='flex items-center gap-4 '>
                 <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Premium</p>
                 <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer'>Install App</p>
                 <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>D</p>
             </div>
         </div>
+
+
         <div className='flex items-center gap-2 mt-4'>
             <p className='bg-white cursor-pointer text-black px-4 py-1 rounded-2xl'>All</p>
             <p className='bg-black cursor-pointer px-4 py-1 rounded-2xl'>Music</p>
